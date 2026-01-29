40937827e514ba811bd72a5b908b60b7ae0f201e
[pherneaspherb] <[johnpherloyd.rangas@wvsu.edu.ph]>
2026-01-29 21:21:55 +0800

    made proxy name to middleware

--- middleware.ts
+++ middleware.ts
@@ -8,7 +8,7 @@ const isPublicRoute = createRouteMatcher([
 
 export default clerkMiddleware(async (auth, req) => {
   if (!isPublicRoute(req)) {
-    await auth.protect()
+    await auth.protect() 
   }
 })
 
