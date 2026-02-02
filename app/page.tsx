051352e2a26bc974f5a71ee650fec2507e45a01b
[pherneaspherb] <[johnpherloyd.rangas@wvsu.edu.ph]>
2026-02-02 17:32:38 +0800

    deleted proxy.ts for awhile

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Navbar from "@/components/navbar";
+
+export default function Home() {
+    return (
+        <div className="min-h-screen bg-linear-to-br from-blue-50 via-white to-purple-50">
+            <Navbar />
+        </div>
+    )
+}
---
+++ components/navbar.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { Trello } from "lucide-react";
+
+export default function Navbar() {
+    return (
+        <header>
+            <div>
+                <div>
+                    <Trello />
+                </div>
+            </div>
+        </header>
+    )
+}
