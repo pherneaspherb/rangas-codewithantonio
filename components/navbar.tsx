b9b077cf63189d7e34f8ed8b9748da2b496a327e
[pherneaspherb] <[johnpherloyd.rangas@wvsu.edu.ph]>
2026-02-04 00:39:38 +0800

    successful http://localhost:3000/dashboard

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { SignInButton, SignUpButton, useUser } from "@clerk/nextjs";
+import { SignInButton, SignUpButton, UserButton, useUser } from "@clerk/nextjs";
 import { ArrowRight, Trello } from "lucide-react";
 import { Button } from "./ui/button";
 import Link from "next/dist/client/link";
@@ -14,7 +14,22 @@ export default function Navbar() {
   const isBoardPage = pathname.startsWith("/boards");
 
   if (isDashboardPage) {
+    return (
+    <header className="border-b bg-white/80 backdrop-blur-sm sticky top-0 z-50">
+      <div className="container mx-2 px-4 py-3 sm:py-4 flex items-center justify-between">
+        <div className="flex items-center space-x-2">
+          <Trello className="h-6 w-6 sm:h-8 sm:w-8 text-blue-600" />
+          <span className="text-xl sm:text-2xl font-bold text-gray-900">
+            Trello Clone
+          </span>
+        </div>
 
+        <div className="flex items-center space-x-2 sm:space-x-4">
+          <UserButton />
+        </div>
+      </div>
+    </header>
+    );
   }
   return (
     <header className="border-b bg-white/80 backdrop-blur-sm sticky top-0 z-50">
