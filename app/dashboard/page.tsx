a11f598f2d18fbce9bbf3495043bf70615e7c4f7
[pherneaspherb] <[johnpherloyd.rangas@wvsu.edu.ph]>
2026-02-04 00:44:35 +0800

    Welcome back greeting with email or firstname

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,21 @@
+"use client";
+
 import Navbar from "@/components/navbar";
+import { useUser } from "@clerk/nextjs";
 
 export default function DashboardPage() {
+    const { user } = useUser();
+    
     return <div className="min-h-screen b-gray-50">
         <Navbar />
+
+        <main className="container mx-auto px-4 py-6 sm:py-8">
+            <div>
+                <h1>
+                    Welcome back,{" "}
+                    {user?.firstName ?? user?.emailAddresses[0].emailAddress}! 👋
+                </h1>
+            </div>
+        </main>
     </div>
 }
