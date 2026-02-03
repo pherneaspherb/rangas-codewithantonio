2becc8c9684f2fa0127d1828c116af97a84c5c64
[pherneaspherb] <[johnpherloyd.rangas@wvsu.edu.ph]>
2026-02-04 00:28:19 +0800

    you can go to http://localhost:3000/dashboard with this

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,22 +1,7 @@
 import Navbar from "@/components/navbar";
-import { useUser } from "@clerk/nextjs";
 
-export default function Dashboard () {
-    const { user } = useUser();
-
-        return (
-        <div className="min-h-screen bg-gray-50">
-            <Navbar />
-
-        <main className="container mx-auto px-4 py-6 sm:py-8">
-            <div className="mb-6 sm:mb-8">
-                <h1 className="text-2xl sm:text-3xl font-bold text-gray-900 mb-2">
-                    Welcome back,{" "}
-                    {user?.firstName ?? user?.emailAddresses[0].emailAddress}!
-                    <p> Here's what'ss happening with your boards today. </p>
-                </h1>
-            </div>
-        </main>
+export default function DashboardPage() {
+    return <div className="min-h-screen b-gray-50">
+        <Navbar />
     </div>
-        );
 }
