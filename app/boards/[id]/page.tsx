03ddad5c2198b66479f0635f00d8a3459ab114c9
John Pher Lloyd Rangas <pherloydrangas@gmail.com>
2026-03-12 10:48:19 +0800

    4 total tasks features are now implemented

--- app/boards/[id]/page.tsx
+++ app/boards/[id]/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import Navbar from "@/components/navbar";
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
@@ -8,10 +9,48 @@ import { Label } from "@/components/ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Textarea } from "@/components/ui/textarea";
 import { useBoard } from "@/lib/hooks/useBoards";
+import { ColumnWithTasks } from "@/lib/supabase/models";
 import { DialogTrigger } from "@radix-ui/react-dialog";
-import { Plus } from "lucide-react";
+import { MoreHorizontal, Plus } from "lucide-react";
 import { useParams } from "next/navigation";
 import { useState } from "react";
+
+function Column({
+    column,
+    children,
+    onCreateTask,
+    onEditColumn,
+}: {
+    column: ColumnWithTasks;
+    children: React.ReactNode;
+    onCreateTask: (taskData: any) => Promise<void>;
+    onEditColumn: (column: ColumnWithTasks) => void;
+}) {
+    return (
+        <div className="w-full lg:flex-shrink-0 lg:w-80">
+            <div className="bg-white rounded-lg shadow-sm border">
+                {/* Column Header */}
+                <div className="p-3 sm:p-4 border-b">
+                    <div className="flex items-center justify-between ">
+                        <div className="flex items-center space-x-2 min-w-0">
+                            <h3 className="font-semibold text-gray-900 text-sm sm:text-base truncate">
+                                {column.title}
+                            </h3>
+                            <Badge variant="secondary" className="text-xs shrink-0">
+                                {column.tasks.length}
+                            </Badge>
+                        </div>
+                        <Button variant="ghost" size="sm" className="shrink-0">
+                            <MoreHorizontal />
+                        </Button>
+                    </div>
+                </div>
+
+                {/* column content */}
+            </div>
+        </div>
+    );
+}
 
 export default function BoardPage() {
     const { id } = useParams<{ id: string }>();
@@ -236,6 +275,14 @@ export default function BoardPage() {
                             </form>
                         </DialogContent>
                     </Dialog>
+                </div>
+
+                {/* Board Columns */}
+                <div>
+                    {columns.map((column, key) => (
+                        <Column key={key} column={column}>
+                        </Column>
+                    ))}
                 </div>
             </main>
         </div>
