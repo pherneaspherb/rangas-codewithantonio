ea0954849d53fccb8e34738dbf2bdf0fa7865f51
[pherneaspherb] <[johnpherloyd.rangas@wvsu.edu.ph]>
2026-04-13 11:14:09 +0800

    after billing, user redirects to dashboard

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -4,7 +4,6 @@ import { PricingTable } from "@clerk/nextjs";
 export default function PricingPage() {
   return (
     <div className="min-h-screen bg-gray-50">
-      <Navbar />
       <div className="py-12 container mx-auto px-4">
         <div className="text-center mb-12">
           <h1 className="text-4xl font-bold text-gray-900 mb-4">
@@ -15,7 +14,7 @@ export default function PricingPage() {
         </p>
         </div>
         <div className="max-w-5xl mx-auto"> 
-            <PricingTable />
+            <PricingTable newSubscriptionRedirectUrl="/dashboard"/>
         </div>
       </div>
     </div>
